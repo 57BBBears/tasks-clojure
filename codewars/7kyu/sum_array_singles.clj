1f98c997f83d83f92e4b7f806b57f3dd9d07c093
57BBBears <seocreomail@gmail.com>
2025-03-11 18:05:27 +0300

    sum of array singles

---
+++ codewars/7kyu/sum_array_singles.clj
@@ -0,0 +1,17 @@
+(ns sum-array-singles
+  (:require [clojure.test :refer [is deftest]])
+  )
+
+(defn sum-singles [nums]
+  (->> nums
+       (filter #(= (filter #{%} nums) [%]))
+       (apply +))
+  )
+
+(sum-singles [1 1 3 2 2 5])
+
+(deftest test-cases
+  (is (= (sum-singles [1 1 3 2 2 5]) 8))
+  (is (= (sum-singles [0 2 2 2 10]) 10))
+  (is (= (sum-singles [-3 1 2 1 2 3 2]) 0))
+  )
