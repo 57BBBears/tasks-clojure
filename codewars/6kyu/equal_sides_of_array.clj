f515648f03d2c35ad21ea16c03e3308237b3e43d
57BBBears <seocreomail@gmail.com>
2025-07-03 17:48:49 +0300

    equal sides of an array

---
+++ codewars/6kyu/equal_sides_of_array.clj
@@ -0,0 +1,21 @@
+(ns equal-sides_of-array
+  (:require [clojure.test :refer [is deftest]]))
+
+(defn find-even-index
+  "Finds the index of an element that is a pivot for an equal sides of an array.
+   Seq[Int] -> Int"
+  [numbers]
+  (loop [i 0]
+  (cond 
+    (= i (count numbers)) -1 
+    (= (apply + (take i numbers)) (apply + (drop (inc i) numbers))) i 
+    :else (recur (inc i))
+    ))
+  )
+
+(deftest test-cases 
+  (is (= (find-even-index [1,2,3,4,3,2,1]), 3))
+  (is (= (find-even-index [1,100,50,-51,1,1]) 1))
+  (is (= (find-even-index [1,2,3,4,5,6]) -1))
+  (is (= (find-even-index [20,10,30,10,10,15,35]) 3))
+  )
